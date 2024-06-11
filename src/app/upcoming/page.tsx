fe4c549346af3ce7cb1efa7dece8cc8f0367fd62
Nitish Singh <nitishsingh925@gmail.com>
2024-06-11 17:17:58 +0530

    add UpcomingMoviesPage

--- src/app/upcoming/page.tsx
+++ src/app/upcoming/page.tsx
@@ -1,7 +1,34 @@
+"use client";
 import React from "react";
+import useUpcomingMovies from "@/hooks/useUpcomingMovies"; // Import the hook for upcoming movies
+import MovieCard from "@/components/MovieCard";
+import { useSelector } from "react-redux";
+import Link from "next/link";
 
-const page = () => {
-  return <div>page this is upcoming</div>;
+const UpcomingMoviesPage = () => {
+  useUpcomingMovies(1); // Fetch upcoming movies with page number 1
+  const { upcomingMovies } = useSelector((store: any) => store.movies);
+  return (
+    <div className="px-6 py-4 bg-neutral-800">
+      <Link href={"/"} className="text-white">
+        Go Back
+      </Link>
+      <h1 className="text-2xl md:text-4xl py-4 text-center text-white">
+        Upcoming Movies
+      </h1>
+      <div className="flex flex-wrap justify-center">
+        {upcomingMovies?.map((movie: any) => (
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            posterPath={movie.poster_path}
+            title={movie.title}
+            release_date={movie.release_date}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default UpcomingMoviesPage;
--- src/hooks/useUpcomingMovies.tsx
+++ src/hooks/useUpcomingMovies.tsx
@@ -3,16 +3,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUpcomingMovies } from "@/lib/features/moviesSlice";
 import { API_TMDB, TMDB_API_OPTIONS } from "@/utils/constants";
 
-const useUpcomingMovies = () => {
+const useUpcomingMovies = (pageNo: number = 1) => {
   const dispatch = useDispatch();
   const upcomingMovies = useSelector(
     (store: any) => store.movies.upcomingMovies
   );
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async (pageNo: number) => {
       try {
-        const res = await fetch(`${API_TMDB}/upcoming`, TMDB_API_OPTIONS);
+        const res = await fetch(
+          `${API_TMDB}/upcoming?language=en-US&page=${pageNo}`,
+          TMDB_API_OPTIONS
+        );
         const data = await res.json();
         dispatch(addUpcomingMovies(data.results));
       } catch (error) {
@@ -21,9 +24,9 @@ const useUpcomingMovies = () => {
     };
 
     if (!upcomingMovies || !upcomingMovies.length) {
-      fetchData();
+      fetchData(pageNo);
     }
-  }, [dispatch, upcomingMovies?.length]);
+  }, [dispatch, pageNo, upcomingMovies?.length]);
 
   return upcomingMovies;
 };
